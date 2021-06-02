498e4b610bacf1e2c1073841d77afa654b0a6670
Maksym Omelianenko <makvitom@gmail.com>
2021-06-02 23:22:49 +0200

    Задание второго урока: присвоение значений переменным и работа с консолью

--- Script/script.js
+++ Script/script.js
@@ -1,5 +1,24 @@
 let money, income, addExpenses, deposit, mission, period;
 
-alert('Тестовое сообщение, нажми "ОК" для продолжения');
-console.log('Следующее тестовое сообщение дял консоли');
+money = 35000; 
+income = 'фотосессия'; 
+addExpenses = 'Налоги, Аренда, Питание, Досуг, Связь, Подарки, Транспорт'; 
+deposit = true; 
+mission = 80000; 
+period = 8;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+console.log('Период равен ', period, ' месяцев');
+console.log('Цель заработать ', mission, ' крон');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+let budgetDay = money/30;
+console.log('budgetDay: ', budgetDay);
+
+
+
 
