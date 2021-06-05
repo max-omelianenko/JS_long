bc08837d44cf286723f86e5950bb7d32a8c325d2
Maksym Omelianenko <makvitom@gmail.com>
2021-06-05 19:20:27 +0200

    Запрос и ввод данных и расчет месячного бюджета

--- Script/script.js
+++ Script/script.js
@@ -1,11 +1,31 @@
 'use strict';
+
 let money = 35000, 
 income = 'бригада', 
 addExpenses = 'Налоги, Аренда, Питание, Досуг, Связь, Подарки, Транспорт', 
 deposit = true, 
 mission = 80000, 
 period = 8,
-budgetDay = money/30;
+budgetDay = money/30,
+compulsoryExpenses1, compulsoryExpenses2, 
+cost1, cost2,
+budgetMonth;
+
+//confirm();
+//prompt();
+money = Number(prompt('Введите ваш месячный доход:'));
+addExpenses = prompt('Перечислите через запятую возможные расходы за расчитываемый период:');
+deposit = confirm('Есть ли у вас депозит в банке?');
+compulsoryExpenses1 = prompt('Введите первую обязательную статью расходов:');
+cost1 = Number(prompt('Введите предполагаемую стоимость:'));
+compulsoryExpenses2 = prompt('Введите вторую обязательную статью расходов:');
+cost2 = Number(prompt('Введите предполагаемую стоимость:'));
+
+budgetMonth = cost1 + cost2;
+
+
+
+
 
 console.log(typeof money);
 console.log(typeof income);
@@ -17,7 +37,7 @@ console.log('Цель заработать ', mission, ' крон');
 console.log(addExpenses.toLowerCase());
 console.log(addExpenses.split(', '));
 console.log('budgetDay: ', budgetDay);
-
+console.log('budgetMonth: ', budgetMonth);
 
 
 
