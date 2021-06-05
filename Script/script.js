29962feaee902a3b7cc2c53ed8723c2db9e8ff82
Maksym Omelianenko <makvitom@gmail.com>
2021-06-05 20:24:35 +0200

    Расчет бюджета на день и реального периода, условия вывода результатов(Задания 7-9)

--- Script/script.js
+++ Script/script.js
@@ -11,33 +11,44 @@ compulsoryExpenses1, compulsoryExpenses2,
 cost1, cost2,
 budgetMonth;
 
-//confirm();
-//prompt();
-money = Number(prompt('Введите ваш месячный доход:'));
-addExpenses = prompt('Перечислите через запятую возможные расходы за расчитываемый период:');
+//Запрос данных от пользователя
+money = Number(prompt('Введите ваш месячный доход:', '45000'));
+addExpenses = prompt('Перечислите через запятую возможные расходы за расчитываемый период:', 'Стоматолог, Курсы, Еда');
 deposit = confirm('Есть ли у вас депозит в банке?');
-compulsoryExpenses1 = prompt('Введите первую обязательную статью расходов:');
-cost1 = Number(prompt('Введите предполагаемую стоимость:'));
-compulsoryExpenses2 = prompt('Введите вторую обязательную статью расходов:');
-cost2 = Number(prompt('Введите предполагаемую стоимость:'));
+compulsoryExpenses1 = prompt('Введите первую обязательную статью расходов:', 'Аренда');
+cost1 = Number(prompt('Введите предполагаемую стоимость:', '17000'));
+compulsoryExpenses2 = prompt('Введите вторую обязательную статью расходов:', 'Налоги');
+cost2 = Number(prompt('Введите предполагаемую стоимость:', '3000'));
 
+//Обработка данных
 budgetMonth = cost1 + cost2;
+period = Math.ceil(mission / (money - budgetMonth));
+budgetDay = Math.floor((money - budgetMonth) / 30);
 
-
-
-
-
+//Вывод результатов в консоль
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
-
 console.log(addExpenses.length);
 console.log('Период равен ', period, ' месяцев');
 console.log('Цель заработать ', mission, ' крон');
 console.log(addExpenses.toLowerCase());
 console.log(addExpenses.split(', '));
-console.log('budgetDay: ', budgetDay);
-console.log('budgetMonth: ', budgetMonth);
+console.log('Бюджет на день: ', budgetDay);
+console.log('Бюджет на месяц: ', budgetMonth);
 
+//Условия вывода
+if (budgetDay > 1200 || budgetDay === 1200) {
+    console.log('У вас высокий уровень дохода');
+} 
+else if (budgetDay > 600 || budgetDay === 600 && budgetDay < 1200) {
+    console.log('У вас средний уровень дохода');
+}
+else if (budgetDay > 0 || budgetDay === 0 && budgetDay < 600){
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+}
+else if (budgetDay < 0){
+    console.log('Что то пошло не так!');
+}
 
 
