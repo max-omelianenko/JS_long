7e15dbef5bbbcf8ae7161fcb8219e9ef5af70d2d
Maksym Omelianenko <makvitom@gmail.com>
2021-08-03 22:43:43 +0200

    калькулятор 24 урока + исправил ошибку 23

--- script/script.js
+++ script/script.js
@@ -277,9 +277,12 @@ window.addEventListener('DOMContentLoaded', () => {
         const calcItems = document.querySelectorAll('.calc-item');
 
         calcItems.forEach((elem) => {
-            elem.addEventListener('blur', (e) => {
-                e.target.value = e.target.value.replace(/\D/g, '');
-            });
+            if (elem.classList.contains('.calc-square') || elem.classList.contains('.calc-count') || 
+            elem.classList.contains('.calc-day')){
+                elem.addEventListener('blur', (e) => {
+                    e.target.value = e.target.value.replace(/\D/g, '');
+                });
+            }
         });
     };
 
@@ -345,5 +348,51 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     InputFiltr();
+
+    //Калькулятор
+    const calc = (price = 100) => {
+
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcCount = document.querySelector('.calc-count'),
+            calcDay = document.querySelector('.calc-day'),
+            totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+            
+            if (calcCount.value > 1){
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5){
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10){
+                dayValue *= 1.5;
+            } 
+
+            if (typeValue && squareValue){
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+            totalValue.textContent = total;
+        };
+
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+
+            if (target.matches('select') || target.matches('input')){
+                countSum();
+            }
+        });
+    };
+
+    calc(100);
+
 
 });
